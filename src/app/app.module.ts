9e32fdeba7e3bd384beec18632bd28cb0b058419
kerniusmatulis <Kernius.matulis@gmail.com>
2018-04-26 15:13:47 +0300

    first init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './admin/services/services.component';
+import { LoginComponent } from './admin/login/login.component';
+import { ServiceDetailsComponent } from './admin/service-details/service-details.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    LoginComponent,
+    ServiceDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
