d1bd7f26ce88c5164c0c822d752890b2b3340eea
Paulius Jarosius <lettell@DESKTOP-OJO0UOU.home>
2018-04-28 16:30:22 +0300

    material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,14 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 // angular material
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 // app components
 import { AppComponent } from './app.component';
 import { ServicesComponent } from './admin/services/services.component';
@@ -35,6 +42,15 @@ import { HomeComponent } from './home/home.component';
     AngularFireAuthModule,
     AngularFirestoreModule,
     BrowserAnimationsModule, // Material...
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule
+
 
   ],
   providers: [],
